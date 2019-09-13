98a2a190a7615372b7dabed187f7db0f647a1290
StevenChen <13631272887@163.com>
2019-09-13 08:59:03 +0800

    '添加taro-ui'

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # my custom framework basic on taro
+
+````
+taro
+redux
+````
+In the project directory, you can run:
+
+only use in weapp
+#### `yarn dev: weapp`
+#### `yarn build: weapp`
--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,8 @@
 import '@tarojs/async-await'
 import Taro, { Component } from '@tarojs/taro'
 import { Provider } from '@tarojs/redux'
-
 import Index from './pages/index'
+import 'taro-ui/dist/style/index.scss'
 
 import configStore from './store'
 
